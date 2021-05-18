1e6514716715d753a79db03535d4c06148daf0f0
jaRue1 <jaruejohnson@outlook.com>
2021-05-18 16:50:59 -0400

    added the users components'

--- src/scenes/Users.js
+++ src/scenes/Users.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
-function Users(params) {
-  return <p>This is User</p>
+function Users() {
+  const [allUsers, setAllUsers] = useState()
+  useEffect(()=>{
+    fetch('http://localhost:5000/users')
+    .then(response => response.json())
+    .then(data => setAllUsers(data))
+    .catch(err => console.log(err))
+  },[])
+  console.log( 'all users' , allUsers)
+  return (
+  <ul class="collection with-header">
+      <li class="collection-header"><h4>All Users</h4></li>
+      {!allUsers ? <h2>Loading...</h2> :allUsers.map(user =>{
+        return (
+        <li class="collection-item">Name: {user.firstName} / Email: {user.email} </li>
+        )
+      })}
+  </ul>
+  )
 }
 export default Users
