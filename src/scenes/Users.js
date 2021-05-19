4d73e8d4afc1cecdeebc4255f4116427e20fdb59
jaRue1 <jaruejohnson@outlook.com>
2021-05-19 00:11:40 -0400

    adding more changes

--- src/scenes/Users.js
+++ src/scenes/Users.js
@@ -1,23 +1,48 @@
 import React, {useEffect, useState} from 'react'
+import { Button } from 'react-materialize'
 
 function Users() {
   const [allUsers, setAllUsers] = useState()
+  const [newUser, setNewUser] = useState({})
   useEffect(()=>{
     fetch('http://localhost:5000/users')
     .then(response => response.json())
     .then(data => setAllUsers(data))
     .catch(err => console.log(err))
   },[])
-  console.log( 'all users' , allUsers)
+  function createUser() {
+    fetch('http://localhost:5000/signup') , { 
+      method:'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({newUser})
+    }
+  }
+  
   return (
+  <div>
   <ul class="collection with-header">
       <li class="collection-header"><h4>All Users</h4></li>
       {!allUsers ? <h2>Loading...</h2> :allUsers.map(user =>{
         return (
-        <li class="collection-item">Name: {user.firstName} / Email: {user.email} </li>
+        <li class="collection-item">Name: {user.firstName} {user.lastName} / Email: {user.email} </li>
         )
       })}
   </ul>
+  
+  <label>First Name</label>
+  <input onChange = {(e) => setNewUser({ ...newUser, firstName: e.target.value})} />
+  <label>Last Name</label>
+  <input onChange = {(e) => setNewUser({ ...newUser, lastName: e.target.value})} />
+  <label>Email</label>
+  <input onChange = {(e) => setNewUser({ ...newUser, email: e.target.value})} />
+  <label>Password</label>
+  <input onChange = {(e) => setNewUser({ ...newUser, password: e.target.value})} />
+
+  <Button onClick = {() => createUser()}>Create User</Button>
+  </div>
   )
 }
 export default Users
